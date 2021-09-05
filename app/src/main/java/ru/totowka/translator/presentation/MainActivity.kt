a2557639c4a57f22c2417adeb5e7bb2ac9aa4384
Tigran <tigran.kocharyan@outlook.com>
2021-09-05 15:42:19 +0300

    add mvvm packages

--- app/src/main/java/ru/totowka/translator/presentation/MainActivity.kt
+++ app/src/main/java/ru/totowka/translator/presentation/MainActivity.kt
@@ -1,9 +1,10 @@
-package ru.totowka.translator
+package ru.totowka.translator.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.totowka.translator.R
 
-class MainActivity : AppCompatActivity() {
+class LauncherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
